cc61160f94a18f152d3e7c580e7b1e7805c8ca83
Miguel Tojal <mtojalduarte@gmail.com>
2025-06-05 16:21:54 -0300

    linkando botoes

--- script.js
+++ script.js
@@ -5,5 +5,25 @@ let navbar = document.querySelector(".navbar");
 menuIcon.onclick = () =>{
     menuIcon.classList.toggle("bx-x");
     navbar.classList.toggle("active");
-    
+
+}
+
+function github(){
+    window.location.href = "https://github.com/mtojald"
+}
+function linkedin(){
+    window.location.href = "https://www.linkedin.com/in/mtojald/"
+}
+function instagram(){
+     window.location.href = "https://www.instagram.com/_mtojal/"
+        }
+
+function projetoWeb(){
+     window.location.href = "https://www.instagram.com/_mtojal/"
+}
+function projetoGaitaHero(){
+     window.location.href = "https://github.com/mtojald/Gaita-Hero"
+}
+function projetoCrud(){
+     window.location.href = "https://github.com/ArthurMatias57/Vida-pet"
 }
